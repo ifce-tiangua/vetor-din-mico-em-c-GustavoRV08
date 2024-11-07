e03a686e028d9af1baee7524e80c2b01ce6aadd7
Gustavo RV <gustavorv102@gmail.com>
2024-11-07 15:17:36 -0300

    Resolvi o exercício sobre vetores

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-
+    int tamanho;
+    int *vetor=NULL;
     //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+    scanf("%d", &tamanho);
+    vetor=(int*) malloc(tamanho*sizeof(int));
+    for(int i=0; i<tamanho; i++){
+        scanf("%d", &vetor[i]);
+    }
+    for(int i=0; i<tamanho; i++){
+        if(i==0){
+            printf("%d", vetor[i]);
+        }else{
+            printf(",%d", vetor[i]);
+        }
+    }
+    free(vetor);
     return 0;
 }
