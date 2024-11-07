8a0fdd52f18378671ac49eea9d40d8c0323d8477
Gustavo RV <gustavorv102@gmail.com>
2024-11-07 15:41:11 -0300

    Resolvi o exercício sobre vetores

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 
 int main(){
     int tamanho;
-    int *vetor=NULL;
+    int *vetor={NULL};
     //insira aqui seu código
     scanf("%d", &tamanho);
     vetor=(int*) malloc(tamanho*sizeof(int));
     for(int i=0; i<tamanho; i++){
         scanf("%d", &vetor[i]);
     }
-    for(int i=0; i<tamanho; i++){
+    if(vetor==NULL){
+        printf("[vazio]\n");
+    }else{
+        for(int i=0; i<tamanho; i++){
         if(i==0){
             printf("'[%d", vetor[i]);
         }else{
             printf(", %d", vetor[i]);
         }
     }
-    printf("]'");
+    }
+    printf("]'\n");
     free(vetor);
     return 0;
 }
